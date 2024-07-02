6c1ccc8ff5ea5054f5640cb48567655d1c3d2c44
shreesha <shreevinayaka977@gmail.com>
2024-07-02 12:04:54 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name='Generative AI Projects',
+    version='0.0.0',
+    author='Shreesha',
+    author_email="shreeshaofficial287@gmail.com",
+    packages=find_packages(),
+    install_requires = []
+)
